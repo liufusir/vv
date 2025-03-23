3629aa243c8a450cbd586f60cf0df16097d3e586
liufusir <52590689+liufusir@users.noreply.github.com>
2025-03-23 16:29:53 +0800

    Update demo.vue

--- docs/.vitepress/components/demo.vue
+++ docs/.vitepress/components/demo.vue
@@ -1,1 +1,32 @@
+<template>
+  <!-- 这里是模板内容，在函数式组件中通常为空 -->
+</template>
 
+<script setup>
+// 定义一个函数式组件
+const d1 = (props, { slots }) => {
+  return (
+    <button type="button">
+      {props.label}
+      {slots.default && slots.default()}
+    </button>
+  );
+};
+const d2 = (props, { slots }) => {
+  return (
+    <p>d2</p>
+  );
+};
+// 定义组件的 props
+ButtonComponent.props = {
+  label: {
+    type: String,
+    default: 'Click me'
+  }
+};
+
+// 导出组件
+defineExpose({
+  d1,d2
+});
+</script>    
